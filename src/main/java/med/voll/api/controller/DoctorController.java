e55e94a3513e9c4d453d7361d723fdf36c8aae93
cmatos <cmatos@uolinc.com>
2024-04-15 19:09:37 -0300

    [master] modify method GET -> list doctors

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -3,29 +3,32 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorDTO;
+import med.voll.api.doctor.DoctorOutputDTO;
 import med.voll.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/doctor")
+@RequestMapping("/doctors")
 public class DoctorController {
 
     @Autowired
     private DoctorRepository doctorRepository;
 
-    @GetMapping
-    public List<Doctor> list() {
-        return doctorRepository.findAll();
-    }
-
     @PostMapping
     @Transactional
-    public void insert(@RequestBody @Valid DoctorDTO doctorDTO) {
-        doctorRepository.save(new Doctor(doctorDTO));
+    public DoctorOutputDTO insert(@RequestBody @Valid DoctorDTO doctorDTO) {
+        Doctor savedDoctor = doctorRepository.save(new Doctor(doctorDTO));
+        return new DoctorOutputDTO(savedDoctor);
+    }
+
+    @GetMapping
+    public List<DoctorOutputDTO> list() {
+        return doctorRepository.findAll().stream().map(DoctorOutputDTO::new).collect(Collectors.toList());
     }
 
 }
