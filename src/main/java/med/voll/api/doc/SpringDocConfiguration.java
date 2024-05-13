295f540e4d87681d69d874ac70192b5c00f31925
cmatos <cmatos@uolinc.com>
2024-05-13 18:35:55 -0300

    [master] add config swagger

--- src/main/java/med/voll/api/doc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/doc/SpringDocConfiguration.java
@@ -2,6 +2,7 @@ package med.voll.api.doc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,8 @@ public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().components(new Components().addSecuritySchemes("bearer-key",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info().title("Voll.med API").description("API para consultas médicas"));
     }
 
 
